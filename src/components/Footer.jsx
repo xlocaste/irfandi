6d01b6f2fd3f0787cd4467260dda0bd7205ad1d7
xlocaste <irfandi.di122@gmail.com>
2024-12-11 15:33:58 +0700

    feat: component folder

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white mt-8">
+      <div className="container mx-auto py-4 px-6 text-center">
+        <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
