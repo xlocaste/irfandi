ba514009486647636b87c88571eb1d26f4ed3524
xlocaste <irfandi.di122@gmail.com>
2024-12-11 16:11:11 +0700

    fix: name file footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white">
-      <div className="container mx-auto py-4 px-6 text-center">
-        <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+    return (
+      <footer className="bg-gray-800 text-white">
+        <div className="container mx-auto py-4 px-6 text-center">
+          <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
