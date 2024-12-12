931d208b5299e817308b3a7db4349d7773c47e39
xlocaste <irfandi.di122@gmail.com>
2024-12-12 12:12:30 +0700

    fix: hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 const Hero = () => {
     return (
-        <div className="flex justify-between">
-            <div>
-                Konten kiri
-            </div>
-            <div>
-                Konten Kanan
+        <div className="flex justify-center p-10">
+            <div className="text-center">
+                <h1 className="text-5xl font-extrabold leading-tight mb-4">
+                    I`m Irfandike
+                </h1>
+                <p className="text-lg mb-6">
+                    I’m a passionate Programmer focused on creating innovative solutions and building the future of technology.
+                </p>
+                <button className="font-semibold border-2 border-black rounded-full py-2 px-4">CONTACT</button>
             </div>
         </div>
     )
