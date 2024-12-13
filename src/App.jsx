b84a68cf4627f91b52d012913bd418332f91b0e9
xlocaste <irfandi.di122@gmail.com>
2024-12-13 13:06:18 +0700

    fix: background 3D

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useRef } from "react";
 import Hero from "./components/Hero";
 
-export default function App() {
-  return (
-    <Hero />
-  )
-}
+const App = () => {
+    const vantaRef = useRef(null);
+
+    useEffect(() => {
+        const VANTA = window.VANTA;
+
+        const vantaEffect = VANTA.NET({
+            el: vantaRef.current,
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.0,
+            minWidth: 200.0,
+            scale: 1.0,
+            scaleMobile: 1.0,
+            color: 0x1e3a8a,
+            backgroundColor: 0x000000,
+        });
+
+        return () => {
+            if (vantaEffect) vantaEffect.destroy();
+        };
+    }, []);
+
+    return (
+      <div className="relative">
+        <div
+            ref={vantaRef}
+            className="fixed top-0 left-0 right-0 bottom-0 z-0"
+        ></div>
+
+        <div className="absolute inset-0 z-10">
+            <Hero />
+        </div>
+    </div>
+    );
+};
+
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,38 +1,11 @@
-import { useEffect, useRef } from "react";
 import Button from "./ButtonStart";
 
 const Hero = () => {
-    const vantaRef = useRef(null);
-
-    useEffect(() => {
-        const VANTA = window.VANTA;
-
-        const vantaEffect = VANTA.NET({
-            el: vantaRef.current,
-            mouseControls: true,
-            touchControls: true,
-            gyroControls: false,
-            minHeight: 200.0,
-            minWidth: 200.0,
-            scale: 1.0,
-            scaleMobile: 1.0,
-            color: 0x1e3a8a,
-            backgroundColor: 0x000000,
-        });
-
-        return () => {
-            if (vantaEffect) vantaEffect.destroy();
-        };
-    }, []);
-
     return (
-        <div
-            ref={vantaRef}
-            className="relative flex justify-center items-center p-10 min-h-screen"
-        >
-            <div className="text-center z-10 text-white">
+        <div className="flex justify-center items-center p-10 min-h-screen text-center">
+            <div className="z-20 text-white">
                 <h1 className="text-5xl font-extrabold leading-tight mb-4">
-                    I`m <a href="" className="text-accent">Irfan</a>di
+                    I`m <a href="" className="text-neutral">Irfan</a>di
                 </h1>
                 <p className="text-lg mb-6">
                     I’m a passionate Programmer focused on creating innovative solutions and building the future of technology.
