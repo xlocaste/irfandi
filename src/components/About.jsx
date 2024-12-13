f55f8e09fa09ebbae8b7945e086da2489e33f3f1
xlocaste <irfandi.di122@gmail.com>
2024-12-13 14:01:32 +0700

    feat: about page app

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 const About = () => {
     return (
-        <div className="min-h-screen flex justify-center flex-col items-center bg-gradient-to-r from-primary to-secondary text-white md:p-20">
+        <div className="min-h-screen flex justify-center flex-col items-center bg-gradient-to-r from-primary to-secondary text-white p-8 md:p-20">
             <p className="text-4xl font-bold mb-10 px-2 text-gradient bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">ABOUT ME</p>
-            <p className="text-center px-32">I am a programmer with over a year of remote work experience as a full-stack web developer based in Indonesia. Throughout my career, I have successfully contributed to various projects, designing and developing both the front-end and back-end of web applications. My passion for technology drives me to continuously learn and stay updated with the latest trends and best practices in software development. I am highly skilled in building responsive, scalable, and efficient web applications, and thrive in collaborative, fast-paced environments. I’m always eager to take on new challenges and find innovative solutions to complex problems.</p>
+            <p className="text-justify md:text-center md:px-32">I am a programmer with over a year of remote work experience as a full-stack web developer based in Indonesia. Throughout my career, I have successfully contributed to various projects, designing and developing both the front-end and back-end of web applications. My passion for technology drives me to continuously learn and stay updated with the latest trends and best practices in software development. I am highly skilled in building responsive, scalable, and efficient web applications, and thrive in collaborative, fast-paced environments. I’m always eager to take on new challenges and find innovative solutions to complex problems.</p>
         </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="flex justify-center items-center p-10 min-h-screen text-center">
             <div className="z-20 text-white">
             <h1 className="text-7xl font-extrabold leading-tight mb-4 text-center">
-                Hi, i’am <a href="" className="text-purple-500">Irfan</a>di
+                Hi, i’am <a href="about" className="text-accent hover:underline">Irfan</a>di
             </h1>
                 <p className="text-lg mb-6">
                     I’m a passionate Programmer focused on creating innovative solutions and building the future of technology.
